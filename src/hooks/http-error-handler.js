2e70fe3e0a32359e9debd15d030fd7dab6190188
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-04-13 20:50:18 +0200

    WIP: use custom hook

---
+++ src/hooks/http-error-handler.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const useHttpErrorHandler = (httpClient) => {
+  const [error, setError] = useState(null);
+
+  const requestInterceptor = httpClient.interceptors.request.use((request) => {
+    setError(null);
+    return request;
+  });
+  const responseInterceptor = httpClient.interceptors.response.use(
+    (response) => response,
+    (err) => {
+      setError(err);
+    }
+  );
+
+  useEffect(() => {
+    return () => {
+      httpClient.interceptors.request.eject(requestInterceptor);
+      httpClient.interceptors.response.eject(responseInterceptor);
+    };
+  }, [requestInterceptor, responseInterceptor]);
+
+  const errorConfirmedHandler = () => {
+    setError(null);
+  };
+
+  return [error, errorConfirmedHandler];
+};
+
+export default useHttpErrorHandler;
