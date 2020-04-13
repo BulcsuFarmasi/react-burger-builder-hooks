f87edc45a244a309646a204279caf3bec41af5fd
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-04-13 20:20:49 +0200

    convert remaining components  to functional component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
+const OrderSummary = (props) => {
   // This could be a functional component, doesn't have to be a class
 
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-          {this.props.ingredients[igKey]}
-        </li>
-      );
-    });
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicous burger with the following ingredients: </p>
-        <ul>{ingredientSummary}</ul>
-        <p>Continue to checkout?</p>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Aux>
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        {props.ingredients[igKey]}
+      </li>
     );
-  }
-}
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicous burger with the following ingredients: </p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout?</p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
 OrderSummary.propTypes = {
   ingredients: PropTypes.object,
   price: PropTypes.number,
   purchaseCancelled: PropTypes.func,
-  purchaseContinued: PropTypes.func
+  purchaseContinued: PropTypes.func,
 };
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import classes from "./Modal.module.css";
@@ -6,35 +6,31 @@ import classes from "./Modal.module.css";
 import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.show !== this.props.show ||
-      nextProps.children !== this.props.children
-    );
-  }
-
-  render() {
-    return (
-      <Aux>
-        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
-          className={classes.Modal}
-          style={{
-            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
-            opacity: this.props.show ? "1" : "0"
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
-}
+const Modal = (props) => {
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
 Modal.propTypes = {
   show: PropTypes.bool,
-  modalClosed: PropTypes.func
+  modalClosed: PropTypes.func,
 };
 
-export default Modal;
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) =>
+    prevProps.show === nextProps.show &&
+    prevProps.children === nextProps.children
+);
